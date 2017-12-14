4f8a62bb5d1c6360bf837493e2b46f5256ca5372
cosmosdsilva93 <33896441+cosmosdsilva93@users.noreply.github.com>
2017-12-14 09:25:50 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<form action="<?php echo 'upload_file.php'; ?>" method="POST" role="form" enctype="multipart/form-data">
 		<legend>Import Data</legend>
 		<div class="form-group">
-			<input type="file" name="file" class="form-control" id="" placeholder="Select a file">
+			<input type="file" name="file[]" class="form-control" id="" multiple="multiple" placeholder="Select a file">
 		</div>
 		<div align="center">
 			<button type="submit" class="btn btn-primary">Upload</button>
--- upload_file.php
+++ upload_file.php
@@ -3,8 +3,20 @@
 	require_once('/config/dbconnect.php');
 
 	if (isset($_FILES)) {
-		$filename = $_FILES['file']['name'];
-		$filepath = $_FILES['file']['tmp_name'];
-		$target =  DOC_ROOT. '/uploads/' . $filename;
-		move_uploaded_file($filepath, $target);
+		$filedata = array();
+		foreach ($_FILES['file']['name'] as $indx => $filename) {
+			$inputFile = file($_FILES['file']['tmp_name'][$indx], FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+			foreach ($inputFile as $line) {
+			    $filedata[$indx][] = explode("\t",$line);
+			}
+		}
+		if (count($filedata) > 0) {
+			foreach ($filedata as $fileIndx => $fileContents) {
+				if ($fileIndx == 0){
+					foreach ($fileContents[0] as $headers) {
+						$columnsArr[] = str_ireplace(' ', '_', strtolower($headers));
+					}
+				}
+			}
+		}
 	}
