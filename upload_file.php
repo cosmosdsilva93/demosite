0062ffd8e5c54e7fef1c6201d389b50c34d6d61d
cosmosdsilva93 <cosmosdsilva93@gmail.com>
2017-12-16 12:37:20 +0530

    1. Removed upload folder and procedure to store files on server to avoid permission issue. 2. Changed the upload file query with insert query to parse the file line by line. 3. Used the multiple inserts in a single query (no. of queries executed = no of selected files) methodology to avoid timeout issues during bulk upload.
    Signed-off-by: cosmosdsilva93 <cosmosdsilva93@gmail.com>

--- upload_file.php
+++ upload_file.php
@@ -5,26 +5,36 @@
 	$msg = '';
 	if (isset($_FILES)) {
 		$filedata = array();
-		$no_of_files = count($_FILES['file']['name']);
-		$i = 0;
+		$total_no_of_files = count($_FILES['file']['name']);
 		//storing all the uploaded files on server
+		$parsedFileCount = 0;
 		foreach ($_FILES['file']['name'] as $indx => $filename) {
-			$target = DOC_ROOT . '/uploads/' . $filename;
-			move_uploaded_file($_FILES['file']['tmp_name'][$indx], $target);
-			$inputFile = file($target, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+			$inputFile = file($_FILES['file']['tmp_name'][$indx], FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 			//since all the files have the same structure so extracting column names from the first file
-			if ($indx == 0) {
-				$headers = explode("\t", $inputFile[0]);
-				foreach ($headers as $headerNames) {
-					$columnsArr[] = str_ireplace(' ', '_', strtolower($headerNames));
+			$dataArr = array();
+			foreach ($inputFile as $lineNo => $lineString) {
+				$line = explode("\t", $lineString);
+				if ($indx == 0 && $lineNo == 0) {
+					foreach ($line as $headerNames) {
+						$columnsArr[] = str_ireplace(' ', '_', strtolower($headerNames));
+					}	
+				} elseif ($lineNo > 0) {
+					$dataArr[] = $line;
 				}
 			}
-			$qry = 'LOAD DATA INFILE "' . $target . '" INTO TABLE ' . TBL_SUBSCRIPTIONS . ' IGNORE 1 LINES (`' . implode('`, `', $columnsArr) . '`)';
-			if ($mysqli->query($qry)) {
-				$i++;
+			if (isset($columnsArr, $dataArr)) {
+				$dbData = array();
+				foreach ($dataArr as $lineData) {
+					$dbData[] =  '("' . implode('", "', $lineData) . '")';	
+				}
+				$qry = 'INSERT INTO ' . TBL_SUBSCRIPTIONS . ' (`' . implode("`, `", $columnsArr) . '`) 
+						VALUES ' . implode(', ', $dbData);
+				if ($mysqli->query($qry)) {
+					$parsedFileCount++;
+				}
 			}
 		}
-		$msg = $indx + 1 . ' out of ' . $no_of_files . ' files imported successfully.';
+		$msg = $parsedFileCount . ' out of ' . $total_no_of_files . ' file(s) imported successfully.';
 	}
 
 	header('location: index.php?msg=' . $msg);
